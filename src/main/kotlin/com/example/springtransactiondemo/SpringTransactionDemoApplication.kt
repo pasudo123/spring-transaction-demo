e2740cbd2a24f6d99218eb77313b6a57a6949e0a
PARK SUNG DONG <oraedoa@gmail.com>
2023-04-03 21:28:41 +0900

    init

---
+++ src/main/kotlin/com/example/springtransactiondemo/SpringTransactionDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springtransactiondemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTransactionDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringTransactionDemoApplication>(*args)
+}
